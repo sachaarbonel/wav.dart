9c6ed06fb29d927bb1273f6ba3dac9fde2950214
Sacha <sacha.arbonel@hotmail.fr>
2019-12-13 23:46:09 +0100

    update readme and doc comments

--- README.md
+++ README.md
@@ -8,10 +8,29 @@ Created from templates made available by Stagehand under a BSD-style
 A simple usage example:
 
 ```dart
+import 'dart:io';
+
 import 'package:wav/wav.dart';
 
-main() {
-  var awesome = new Awesome();
+main() async {
+  final wav = await WavReader.open(file: File('oishii.wav'));
+  print(wav.chunkID);
+  print(wav.chunkSize);
+  //final WavFormat format;
+  print(wav.format);
+  print(wav.subChunk1ID);
+  print(wav.subChunk1Size);
+  print(wav.encoding);
+  // final Encoding encoding;
+  print(wav.numChannels);
+  print(wav.sampleRate);
+  print(wav.blockAlign);
+  print(wav.bitsPerSample);
+  print(wav.subChunk2ID);
+  print(wav.subChunk2Size);
+  print(wav.bytesPerSample);
+  print(wav.sampleCount);
+  print(wav.audioLength);
 }
 ```
 
--- lib/src/wav_base.dart
+++ lib/src/wav_base.dart
@@ -3,6 +3,33 @@ import 'dart:io';
 enum WavFormat { WAVE }
 enum Encoding { PCM }
 
+/// Returns the lesser of two numbers.
+///
+/// ```dart
+/// import 'dart:io';
+/// import 'package:wav/wav.dart';
+/// main() async {
+///   final wav = await WavReader.open(file: File('oishii.wav'));
+///   print(wav.chunkID);
+///   print(wav.chunkSize);
+///   ///final WavFormat format;
+///   print(wav.format);
+///   print(wav.subChunk1ID);
+///   print(wav.subChunk1Size);
+///   print(wav.encoding);
+///   /// final Encoding encoding;
+///   print(wav.numChannels);
+///   print(wav.sampleRate);
+///   print(wav.blockAlign);
+///   print(wav.bitsPerSample);
+///   print(wav.subChunk2ID);
+///   print(wav.subChunk2Size);
+///   print(wav.bytesPerSample);
+///   print(wav.sampleCount);
+///   print(wav.audioLength);
+/// }
+/// ```
+
 class WavReader {
   final String chunkID;
   final int chunkSize;
